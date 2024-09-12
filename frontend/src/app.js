1a5056a4d1deccbbcbc496c9497bdd5dadc29721
sa-gems-github <sa-gems-github@dfinity.org>
2024-09-12 09:21:46 -0400

    0191e665-3d46-7aa8-a8b9-c8f02c8be7b9

---
+++ frontend/src/app.js
@@ -0,0 +1,11 @@
+// This file is currently empty but can be used for future JavaScript functionality
+
+// Example of how to interact with the backend:
+// import { backend } from 'declarations/backend';
+
+// async function greet() {
+//     const greeting = await backend.greet("World");
+//     console.log(greeting);
+// }
+
+// greet();
